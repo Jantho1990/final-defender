1c18a76cdfebf8c7b602b92fe4f14e5deb668e99
Joshua Anthony <jantho1990@gmail.com>
2019-06-29 12:04:20 -0500

    added downward movement; added a Bullet class; player can now fire bullets

---
+++ src/entities/Bullet.js
@@ -0,0 +1,29 @@
+import Phaser from 'phaser'
+
+export default class Bullet extends Phaser.GameObjects.Image {
+  constructor (scene) {
+    super(scene, 0, 0, 'bullet')
+
+    this.speed = 0
+    this.born = 0
+  }
+
+  fire (player) {
+    this.setPosition(player.x, player.y)
+
+    this.speed = Phaser.Math.GetSpeed(1000 + player.vel.x, 1)
+
+    this.born = 0
+  }
+
+  update (time, delta) {
+    this.x += this.speed * delta
+
+    this.born += delta
+
+    if (this.born > 1000) {
+      this.setActive(false)
+      this.setVisible(false)
+    }
+  }
+} 
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,8 +1,10 @@
 import Phaser from 'phaser'
 import logoImg from "../assets/logo.png";
 // import Player from '../entities/player'
+import Bullet from '../entities/Bullet'
 
 import playerImg from '../assets/img/spaceship.png'
+import bulletImg from '../assets/img/laser-bullet.png'
 
 export default class GameScene extends Phaser.Scene {
     constructor () {
@@ -12,10 +14,17 @@ export default class GameScene extends Phaser.Scene {
     preload() {
       this.load.image("logo", logoImg);
       this.load.image('spaceship', playerImg)
+      this.load.image('bullet', bulletImg)
     }
 
     create() {
       const logo = this.add.image(400, 150, "logo");
+      
+      this.bullets = this.add.group({
+        classType: Bullet,
+        runChildUpdate: true
+      })
+      this.lastFired = 0
       
       // this.add.sprite()
       // this.player = new Player(this, { x: 100, y: 100 })
@@ -37,12 +46,33 @@ export default class GameScene extends Phaser.Scene {
 
     update (time, delta) {
       const { cursors, player } = this
+
       if (cursors.right.isDown) {
         player.setAccelerationX(800)
       } else if (cursors.left.isDown) {
         player.setAccelerationX(-800)
       } else {
         player.setAccelerationX(0)
+      }
+
+      if (cursors.up.isDown) {
+        player.setAccelerationY(-800)
+      } else if (cursors.down.isDown) {
+        player.setAccelerationY(800)
+      } else {
+        player.setAccelerationY(0)
+      }
+
+      if (cursors.space.isDown && time > this.lastFired) {
+        let bullet = this.bullets.get()
+        bullet.setActive(true)
+        bullet.setVisible(true)
+        console.log(bullet)
+
+        if (bullet) {
+          bullet.fire(player)
+          this.lastFired = time + 100
+        }
       }
     }
 }
